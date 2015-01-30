9457b3e543a055bf5e78ff9fdef4506ec81de03d
andreas1992 <aks1992@gmail.com>
2015-01-30 13:01:29 +0100

    Adder metode for å konvertere fra romertall til desimaltall

--- Lab2.py
+++ Lab2.py
@@ -47,3 +47,35 @@ def toRoman(n):
 # Til slutt hopper den over en haug med tall til den kommer til 4 for IV, og dermed har den "tømt" n for tall
 # som nå kun står igjen med 0, og 0 har ingen verdi i romertall.
 # Dermed inneholder result nå DCCCXCIV. result = "" over gjør at den returnerer en string.
+
+# Define pattern to detect valid Roman numerals
+romanNumeralPattern = re.compile("""
+    ^                   # beginning of string                                
+    M{0,4}              # thousands - 0 to 4 M's
+    (CM|CD|D?C{0,3})    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
+                        #            or 500-800 (D, followed by 0 to 3 C's)
+    (XC|XL|L?X{0,3})    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
+                        #        or 50-80 (L, followed by 0 to 3 X's)
+    (IX|IV|V?I{0,3})    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
+                        #        or 5-8 (V, followed by 0 to 3 I's)
+    $                   # end of string
+    """ ,re.VERBOSE)
+    
+# re.VERBOSE er et såkalt 'Flag' i metoden som gjør at man kan skrive
+# inn ting i metodekallet mer oversiktlig, slikt som kommentarer, samt
+# at den ignorerer whitespaces.
+
+def fromRoman(s):
+    """convert Roman numeral to integer"""
+    if not s:
+        raise InvalidRomanNumeralError, 'Input can not be blank'
+    if not romanNumeralPattern.search(s):
+        raise InvalidRomanNumeralError, 'Invalid Roman numeral: %s' % s
+
+    result = 0
+    index = 0
+    for numeral, integer in romanNumeralMap:
+        while s[index:index+len(numeral)] == numeral:
+            result += integer
+            index += len(numeral)
+    return result
