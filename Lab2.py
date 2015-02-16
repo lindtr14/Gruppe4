1e5534d34e65890e25be011b5b6acbd7566daeaf
olea14 <olea14@student.uia.no>
2015-02-16 13:18:22 +0100

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -32,25 +32,25 @@ def toRoman(n):
             n -= integer
     return result
 
-"""Metoden forklart: Ved input går for-løkken gjennom ordboka og
-henter ut numeral (romertegn) og integer (desimaltall).
+    """Metoden forklart: Ved input går for-løkken gjennom ordboka og
+    henter ut numeral (romertegn) og integer (desimaltall).
 
-I for-løkken kjøres så en while-løkke som først sammenlikner n (input) med integer (tall i ordboka),
-# og ser om n er større eller lik integer.
-# Med 894 som eksempel tar den da og ser at tallet ikke er større eller lik første i listen, nemlig 1000 for M.
-# Dermed kjøres løkken med en gang om igjen, og den sammenligner så med neste, som er 900. Igjen er n mindre,
-# så derfor går den på nytt videre og ser om neste er lik eller mindre.
-# Da finner den 500 eller D, som jo ER mindre enn n (894).
-# Dermed kjøres while-løkken og numeral(romertall) legges til variabelen result,
-# slik at result så langt har D for 500.
-# Deretter trekkes 500 fra n, (n -= integer) og n har nå tallet 394. Videre ser vi at det er lavere enn 400 for CD.
-# Deretter ser man at den vil gå 3 ganger gjennom while-løkken siden 394 har 3 "hundrere".
-# Så plusses de på i result, som da blir DCCC.
-# Nå har n bare 94, og ved neste runde blir det 4 da 94 er større enn 90 for XC.
-# Da har result romertallet DCCCXC.
-# Til slutt hopper den over en haug med tall til den kommer til 4 for IV, og dermed har den "tømt" n for tall
-# som nå kun står igjen med 0, og 0 har ingen verdi i romertall.
-# Dermed inneholder result nå DCCCXCIV. result = "" over gjør at den returnerer en string"""
+    I for-løkken kjøres så en while-løkke som først sammenlikner n (input) med integer (tall i ordboka),
+    # og ser om n er større eller lik integer.
+    # Med 894 som eksempel tar den da og ser at tallet ikke er større eller lik første i listen, nemlig 1000 for M.
+    # Dermed kjøres løkken med en gang om igjen, og den sammenligner så med neste, som er 900. Igjen er n mindre,
+    # så derfor går den på nytt videre og ser om neste er lik eller mindre.
+    # Da finner den 500 eller D, som jo ER mindre enn n (894).
+    # Dermed kjøres while-løkken og numeral(romertall) legges til variabelen result,
+    # slik at result så langt har D for 500.
+    # Deretter trekkes 500 fra n, (n -= integer) og n har nå tallet 394. Videre ser vi at det er lavere enn 400 for CD.
+    # Deretter ser man at den vil gå 3 ganger gjennom while-løkken siden 394 har 3 "hundrere".
+    # Så plusses de på i result, som da blir DCCC.
+    # Nå har n bare 94, og ved neste runde blir det 4 da 94 er større enn 90 for XC.
+    # Da har result romertallet DCCCXC.
+    # Til slutt hopper den over en haug med tall til den kommer til 4 for IV, og dermed har den "tømt" n for tall
+    # som nå kun står igjen med 0, og 0 har ingen verdi i romertall.
+    # Dermed inneholder result nå DCCCXCIV. result = "" over gjør at den returnerer en string"""
 
 # Define pattern to detect valid Roman numerals
 romanNumeralPattern = re.compile("""
@@ -76,13 +76,13 @@ def fromRoman(s):
     if not romanNumeralPattern.search(s):
         raise InvalidRomanNumeralError, 'Invalid Roman numeral: %s' % s
 
-""" For-løkken går gjennom hvert nøkkel-verdipar med romertall og desimalverdier.
-Index = 0, og While-løkken bruker derfor slice fra venstre til høyre og deler
-stringen 's' opp i biter som passer til nøklene (romertallene) og legger de
-tilsvarende verdiene (desimaltall) til result. Avhengig av lengden til numeral, vokser index
-med 1 eller 2 for hver gang while-løkken kjører, og programmet tar dermed for seg romertallet
-bit for bit til hele tallet er lest. Når s[index:index+len(numeral)] ikke lengre tilsvarer en numeral (nøkkel)
-er while-løkken ferdig og result viser verdien i desimaltall."""
+    """ For-løkken går gjennom hvert nøkkel-verdipar med romertall og desimalverdier.
+    Index = 0, og While-løkken bruker derfor slice fra venstre til høyre og deler
+    stringen 's' opp i biter som passer til nøklene (romertallene) og legger de
+    tilsvarende verdiene (desimaltall) til result. Avhengig av lengden til numeral, vokser index
+    med 1 eller 2 for hver gang while-løkken kjører, og programmet tar dermed for seg romertallet
+    bit for bit til hele tallet er lest. Når s[index:index+len(numeral)] ikke lengre tilsvarer en numeral (nøkkel)
+    er while-løkken ferdig og result viser verdien i desimaltall."""
 
 
 result = 0
