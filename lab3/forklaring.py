f7dc3e3c17e9ad871b0ce22e39b3b61327831805
andreas1992 <aks1992@gmail.com>
2015-03-06 13:26:51 +0100

    legger til forklaring til oppgave 3

---
+++ lab3/forklaring.py
@@ -0,0 +1,10 @@
+# Forklaring til Oppgave 3.
+
+#   endring fra stor til liten bokstav har den sammenhengen at:
+#   feks Y = 01011001
+#   og   y = 01111001
+
+#   plass nummer 5 (1/0*2^5) skrus på når den skal vise liten bokstav 
+#   og er av når den skal representere stor bokstav.
+#   med andre ord legges det til 32 i desimaltallverdi når man går fra 
+#   stor bokstav til liten bokstav.
