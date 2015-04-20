c82554df0923230248d1ff687e5608b0741bfcf7
Andreas <aks1992@gmail.com>
2015-04-20 15:37:02 +0200

    Gjør at man kan konvertere fra liten til stor bokstav i unicode

--- lab3/UDPServer.py
+++ lab3/UDPServer.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from socket import *
 
 serverPort = 12000
@@ -7,5 +9,6 @@ serverSocket.bind(('', serverPort))
 print "The server is ready to recieve"
 while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.upper()
-    serverSocket.sendto(modifiedMessage, clientAddress)
+    modifiedMessage = message.decode('utf-8').upper()
+    unicodeModifiedMessage = modifiedMessage.encode('utf-8')
+    serverSocket.sendto(unicodeModifiedMessage, clientAddress)
