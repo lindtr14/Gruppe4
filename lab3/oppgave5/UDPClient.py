ea3d398efe43a27061978d02674e3ef7e05e7d9d
lindtr14 <lindtr14@student.uia.no>
2015-05-07 17:26:13 +0200

    Update UDPClient.py

--- lab3/oppgave5/UDPClient.py
+++ lab3/oppgave5/UDPClient.py
@@ -5,8 +5,18 @@ from socket import *
 serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message  = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message,(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage
-clientSocket.close()
+clientSocket.settimeout(2)
+
+connectionMessage = "Is connection valid?"
+clientSocket.sendto(connectionMessage,(serverName, serverPort))
+modifiedConnectionMessage, serverAddress = clientSocket.recvfrom(2048)
+
+if modifiedConnectionMessage == "Yes":
+	print "Connection valid"
+	message  = raw_input('Input lowercase sentence:')
+	clientSocket.sendto(message,(serverName, serverPort))
+	message, serverAddress = clientSocket.recvfrom(2048)
+
+	print modifiedMessage
+else:
+	clientSocket.close()
