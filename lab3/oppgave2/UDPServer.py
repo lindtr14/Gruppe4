e0b9dc060e0d4cd44b53461148f48792328ba5ad
lindtr14 <lindtr14@student.uia.no>
2015-05-07 18:44:08 +0200

    Update UDPServer.py

--- lab3/oppgave2/UDPServer.py
+++ lab3/oppgave2/UDPServer.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-
 from socket import *
 
-serverName = '127.0.0.1'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message  = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message,(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage
-clientSocket.close()
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((’’, serverPort))
+print ”The server is ready to receive”
+
+while 1:
+message, clientAddress = serverSocket.recvfrom(2048)
+modifiedMessage = message.upper()
+serverSocket.sendto(modifiedMessage, clientAddress)
