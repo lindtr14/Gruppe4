465205f879d04fd3c145055f26c97814b8769834
lindtr14 <lindtr14@student.uia.no>
2015-05-07 18:55:55 +0200

    Update UDPServerNy.py

--- lab3/oppgave5/UDPServerNy.py
+++ lab3/oppgave5/UDPServerNy.py
@@ -24,34 +24,34 @@ def asciiBin(character):
 
 def binaryChange(binary):
 
-	listBinary = list(binary)
-	listBinary[2] = "0"
-	return "".join(listBinary)
+    listBinary = list(binary)
+    listBinary[2] = "0"
+    return "".join(listBinary)
 
 def fromBinaryToText(newBinary):
 
-	return chr(int(newBinary, 2))
+    return chr(int(newBinary, 2))
 
 def test():
-	assert asciiBin("a") == "01100001"
-	assert binaryChange("01100001") == "01000001"
-	assert fromBinaryToText("01000001") == "A"
+    assert asciiBin("a") == "01100001"
+    assert binaryChange("01100001") == "01000001"
+    assert fromBinaryToText("01000001") == "A"
 
-	return "Testene er fullført uten feil"
+    return "Testene er fullført uten feil"
 
 print test()
 
 
 def get_binary(number):
-	return format(number, 'b').zfill(8)
+    return format(number, 'b').zfill(8)
 
 def set_bit(v, index, x):
-	"""Set the index:th bit of v to x, and return the new value."""
-	mask = 1 << index
-	v &= ~mask
-	if x:
-		v |= mask
-	return v
+    """Set the index:th bit of v to x, and return the new value."""
+    mask = 1 << index
+    v &= ~mask
+    if x:
+        v |= mask
+    return v
 
 loop = True
 
@@ -60,10 +60,11 @@ while loop:
     if connectionMessage == "Is connection valid?":
         positiveMessage = "Yes"
         serverSocket.sendto(positiveMessage, clientAddress)
+        loop = False
 
 
 while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
-    changed_bin = set_bit(ord(message), 5, 0)	# Gjør om på binærtallet, stor bokstav
-    uppercase = chr(changed_bin)	# Gjør om til ascii
+    changed_bin = set_bit(ord(message), 5, 0)   # Gjør om på binærtallet, stor bokstav
+    uppercase = chr(changed_bin)    # Gjør om til ascii
     serverSocket.sendto(uppercase, clientAddress)
