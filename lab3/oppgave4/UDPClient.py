c370775e23716c3ab2e2329db65462aed163d27c
lindtr14 <lindtr14@student.uia.no>
2015-05-07 17:37:11 +0200

    Update UDPClient.py

--- lab3/oppgave4/UDPClient.py
+++ lab3/oppgave4/UDPClient.py
@@ -5,7 +5,7 @@ from socket import *
 serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message  = raw_input('Input lowercase letter:')
+message  = raw_input('Input lowercase ascii letter:')
 clientSocket.sendto(message,(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print modifiedMessage
