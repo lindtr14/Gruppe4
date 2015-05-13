a69f82d31aae3d18c4fb506c974b5677261f583a
lindtr14 <lindtr14@student.uia.no>
2015-05-13 13:15:52 +0200

    Update UDPServerNy.py

--- lab3/oppgave5/UDPServerNy.py
+++ lab3/oppgave5/UDPServerNy.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+# This module acts like the server that
+# recieves a message from the client and
+# processes it from lowercase to uppercase by turning the
+# required bit to make it happen
+# It then sends the uppercase version to the client
+
 from socket import *
 
 serverPort = 12000
