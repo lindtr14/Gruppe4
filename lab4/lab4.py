76ddfde84f3d74594dfcae7ca7389dcb2eb10413
andreas1992 <aks1992@gmail.com>
2015-05-22 21:30:16 +0200

    Update lab4.py

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -127,16 +127,16 @@ def test():
 	
 	a = [2, 2, 2, 3, 3]
 	b = [2, 2, 3, 3, 7]
+	test_hands = [['6S', 'JD', 'TD', 'QS', '5H'], ['7D', '2S', 'AC', '8D', '4H'],
+                      ['7C', '7H', 'KD', 'KS', '2D'], ['5S', '8C', 'AD', 'AS', '4D'], 
+                      ['5D', 'KC', 'JS', '9S', '8H']]
+        after_hand_rank = [(0, (12, 11, 10, 6, 5)), (0, (14, 8, 7, 4, 2)), (2, (13, 7, 2)), 
+    			   (1, (14, 8, 5, 4)), (0, (13, 11, 9, 8, 5))]
 	
 	assert card_ranks(['AS', '4S', '5C', '2D', 'KH']) == [14, 13, 5, 4, 2]
-	assert allmax([(0, (12, 11, 10, 6, 5)), (0, (14, 8, 7, 4, 2)), (2, (13, 7, 2)), 
-                 (1, (14, 8, 5, 4)), (0, (13, 11, 9, 8, 5))]) == [(2, (13, 7, 2))]
-  	assert allmax([['6S', 'JD', 'TD', 'QS', '5H'], ['7D', '2S', 'AC', '8D', '4H'],
-  		['7C', '7H', 'KD', 'KS', '2D'], ['5S', '8C', 'AD', 'AS', '4D'], 
-	 	['5D', 'KC', 'JS', '9S', '8H']], key=hand_rank) == [['7C', '7H', 'KD', 'KS', '2D']]
-	assert poker([['6S', 'JD', 'TD', 'QS', '5H'], ['7D', '2S', 'AC', '8D', '4H'],
-	           ['7C', '7H', 'KD', 'KS', '2D'], ['5S', '8C', 'AD', 'AS', '4D'], 
-	           ['5D', 'KC', 'JS', '9S', '8H']]) == [['7C', '7H', 'KD', 'KS', '2D']]
+	assert allmax(after_hand_rank) == [(2, (13, 7, 2))]
+  	assert allmax(test_hands, key=hand_rank) == [['7C', '7H', 'KD', 'KS', '2D']]
+	assert poker(test_hands) == [['7C', '7H', 'KD', 'KS', '2D']]
 	assert hand_rank(['7C', '7H', 'KD', 'KS', '2D']) == (2, (13, 7, 2))
 	assert kind(1, a) == None
 	assert kind(2, a) == 3
