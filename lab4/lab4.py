2033042c16529e7814259c3b1cce9e780ec9ee76
andreas1992 <aks1992@gmail.com>
2015-05-22 21:48:25 +0200

    Update lab4.py

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,70 +1,52 @@
 # -*- coding: utf-8 -*-
 
-#   Takes all the hands made by the deal function. The hands comes as
-#   a list of lists (hands). Then every hand will be sorted and given
-#   a rank (hand_rank) in turn, one after the other.
-#   The allmax function will then find wich one is the highest ranking
-#   hand or hands if there is a tie.
+# This module represents a game of poker with players. 
+# 
+
 def poker(hands):
   return allmax(hands, key=hand_rank)
   
 def hand_rank(hand):
   groups = group(['--23456789TJQKA'.index(r) for r, s in hand])
   counts, ranks = unzip(groups)
-  # makes straight with ace valued to one possible.
+
   if ranks == (14, 5, 4, 3, 2):
     ranks = (5, 4, 3, 2, 1) 
-  # is straight if 5 different ranks, and the highest rank is 4 higher than lowest rank
-  # (a neat way of checking for 5 consecutive ranks)
+
   straight = len(ranks) == 5 and max(ranks)-min(ranks) == 4
-  # is flush if there is only one suit in the set of suits
+
   flush = len(set([s for r, s in hand])) == 1
   count_rankings = {(5,):10, (4,1):7, (3,2):6, (3,1,1):3, (2,2,1):2, (2,1,1,1):1, (1,1,1,1,1):0}
   return max(count_rankings[counts], 4*straight + 5*flush), ranks
 
-def group(items): # Takes a list of items and returns a list of "counts" of items and the list itself
-  groups = [(items.count(x), x) for x in set(items)] # Goes through set(items) and returns pairs of the "count" and set and sorts them in reversed order so that the highest comes first
+def group(items):
+  groups = [(items.count(x), x) for x in set(items)] 
   return sorted(groups, reverse=True) 
   
-def unzip(pairs): return zip(*pairs) # Converts a list of pairs into a pair of lists 
+def unzip(pairs): return zip(*pairs)
   
-#Takes a hand, pulls out a ranking of each hands, 
-#sorts them so the highest number is first and return them.
 def card_ranks(hand):
-  #Return a list of  the ranks, sorted with the higher first.
+
   ranks = ["--23456789TJQKA".index(r) for r,s in hand]
-  #Reverse the rank. Because we usually associate highest number (8) with the best cards.
   ranks.sort(reverse=True)
-  #If ranks is equal to ace low/high straight, it will return a fix, this is the only time returning 1 as a value
-	#Makes ace to be one instead of fourteen. 
-	#If any other ranks, return as it.
+
   return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
-  
-  
-# n is the input for what kind of rank «kind» we want to find. 2 for two of a kind (1pair), 
-# 3 for three of a kind, 4 for four of a kind
-# ranks will be our hand or the list of card we got
 
 def kind(n, ranks):
-  
-# this for expression will check in our ranks/list if it can count how many r are alike, depending on what number we defined in n
-# if we said n = 3, the for expression will try to find if it can find 3 r’s that are the same. 
-# If it is, it will return that r value. If not it will return none
-# count is a method in lists
 
   for r in ranks:
     if ranks.count(r) == n: return r
   return None
     
 def two_pair(ranks):
-  pair = kind(2, ranks) # Finding two cards that has the same value. Give them the name "pair". 
-                        # This will pick out the highest pair first
-  lowpair = kind(2, list(reversed(ranks))) # Finding two cards that has the same value, but in a reversed list. 
-                                           # Since its reversed, this will pick out the lowest pair
-  if pair and lowpair != pair: # Checking if pair and lowpair is NOT the same
-    return (pair, lowpair) # If the "if" is true, then return their values
+  pair = kind(2, ranks)
+  
+  lowpair = kind(2, list(reversed(ranks))) 
+  
+  if pair and lowpair != pair: 
+    return (pair, lowpair)
   else:
-    return None # Else return None
+    return None 
   
 
 def allmax(iterable, key=(lambda x: x)):
@@ -80,7 +62,6 @@ def allmax(iterable, key=(lambda x: x)):
       result.append(x)
   return result 
   
-# importing the module random to help us pick out random cards from the deck
 import random
 
 def deal(numhands, n=5, deck=[r+s for r in '23456789TJQKA' for s in 'SHDC']):
