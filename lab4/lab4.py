38b89b2d81b22c06bed2fd1a6b8f00798838882d
andreas1992 <aks1992@gmail.com>
2015-05-22 22:10:47 +0200

    Update lab4.py

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -24,7 +24,8 @@ def hand_rank(hand):
   straight = len(ranks) == 5 and max(ranks)-min(ranks) == 4
 
   flush = len(set([s for r, s in hand])) == 1
-  count_rankings = {(5,):10, (4,1):7, (3,2):6, (3,1,1):3, (2,2,1):2, (2,1,1,1):1, (1,1,1,1,1):0}
+  count_rankings = {(5,):10, (4,1):7, (3,2):6, (3,1,1):3, 
+  			            (2,2,1):2, (2,1,1,1):1, (1,1,1,1,1):0}
   return max(count_rankings[counts], 4*straight + 5*flush), ranks
 
 def group(items):
@@ -68,8 +69,9 @@ def two_pair(ranks):
   
 
 def allmax(iterable, key=(lambda x: x)):
-  """ Compares all hands in a list of lists (hands) and finds the one with
-      the maximum value or rank. If there is a tie, both hands will be returned.
+  """ Compares all hands in a list of lists (hands) and 
+      finds the one with the maximum value or rank. 
+      If there is a tie, both hands will be returned.
   """
   result, maxval = [], None
   for x in iterable:
@@ -82,9 +84,11 @@ def allmax(iterable, key=(lambda x: x)):
   
 import random
 
-def deal(numhands, n=5, deck=[r+s for r in '23456789TJQKA' for s in 'SHDC']):
-  """ Deals hands for players. Each hand consist of 5 cards that are randomly
-      picked out of the shuffeled deck. Numhands refer to number of hands.
+def deal(numhands, n=5, 
+          deck=[r+s for r in '23456789TJQKA' for s in 'SHDC']):
+  """ Deals hands for players. Each hand consist of 5 cards 
+      that are randomly picked out of the shuffeled deck. 
+      Numhands refer to number of hands.
   """
   random.shuffle(deck)
   return [deck[n*i:n*(i+1)] for i in range(numhands)]
@@ -94,15 +98,20 @@ def test():
 	
 	a = [2, 2, 2, 3, 3]
 	b = [2, 2, 3, 3, 7]
-	test_hands = [['6S', 'JD', 'TD', 'QS', '5H'], ['7D', '2S', 'AC', '8D', '4H'],
-                      ['7C', '7H', 'KD', 'KS', '2D'], ['5S', '8C', 'AD', 'AS', '4D'], 
-                      ['5D', 'KC', 'JS', '9S', '8H']]
-        hand_values = [(0, (12, 11, 10, 6, 5)), (0, (14, 8, 7, 4, 2)), (2, (13, 7, 2)), 
-    			   (1, (14, 8, 5, 4)), (0, (13, 11, 9, 8, 5))]
+	test_hands = [['6S', 'JD', 'TD', 'QS', '5H'], 
+	              ['7D', '2S', 'AC', '8D', '4H'],
+                ['7C', '7H', 'KD', 'KS', '2D'], 
+                ['5S', '8C', 'AD', 'AS', '4D'], 
+                ['5D', 'KC', 'JS', '9S', '8H']]
+  hand_values = [(0, (12, 11, 10, 6, 5)), (0, (14, 8, 7, 4, 2)), 
+                  (2, (13, 7, 2)), (1, (14, 8, 5, 4)), 
+                  (0, (13, 11, 9, 8, 5))]
 	
-	assert card_ranks(['AS', '4S', '5C', '2D', 'KH']) == [14, 13, 5, 4, 2]
+	assert card_ranks(['AS', '4S', '5C', '2D', 'KH']) == 
+	                  [14, 13, 5, 4, 2]
 	assert allmax(hand_values) == [(2, (13, 7, 2))]
-  	assert allmax(test_hands, key=hand_rank) == [['7C', '7H', 'KD', 'KS', '2D']]
+  	assert allmax(test_hands, key=hand_rank) == 
+  	                [['7C', '7H', 'KD', 'KS', '2D']]
 	assert poker(test_hands) == [['7C', '7H', 'KD', 'KS', '2D']]
 	assert hand_rank(['7C', '7H', 'KD', 'KS', '2D']) == (2, (13, 7, 2))
 	assert kind(1, a) == None
@@ -111,7 +120,8 @@ def test():
 	assert kind(4, a) == None
 	assert two_pair(b) == (2, 3)
 	assert unzip([(3, 2), (1, 5), (1, 3)]) == [(3, 1, 1), (2, 5, 3)]
-	assert group([2, 14, 5, 12, 5]) == [(2, 5), (1, 14), (1, 12), (1, 2)]
+	assert group([2, 14, 5, 12, 5]) == 
+	              [(2, 5), (1, 14), (1, 12), (1, 2)]
 	
 	return "The test was a success"
 
